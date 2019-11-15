7f0c7277d8a9808770874abf7156c729ecf75572
Andreas Adi Purwanto <andreas1@mhs.stts.edu>
2019-11-15 15:46:15 +0700

    Tambahan Form List_Barang

---
+++ Proyek ACS/List_Barang.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Proyek_ACS
+{
+    partial class List_Barang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ID_BARANG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NAMA_BARANG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DETAIL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID_BARANG,
+            this.NAMA_BARANG,
+            this.DETAIL});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(970, 377);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(279, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(570, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(879, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(879, 494);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // ID_BARANG
+            // 
+            this.ID_BARANG.HeaderText = "ID_BARANG";
+            this.ID_BARANG.MinimumWidth = 6;
+            this.ID_BARANG.Name = "ID_BARANG";
+            this.ID_BARANG.ReadOnly = true;
+            // 
+            // NAMA_BARANG
+            // 
+            this.NAMA_BARANG.HeaderText = "NAMA_BARANG";
+            this.NAMA_BARANG.MinimumWidth = 6;
+            this.NAMA_BARANG.Name = "NAMA_BARANG";
+            this.NAMA_BARANG.ReadOnly = true;
+            // 
+            // DETAIL
+            // 
+            this.DETAIL.HeaderText = "DETAIL";
+            this.DETAIL.MinimumWidth = 6;
+            this.DETAIL.Name = "DETAIL";
+            this.DETAIL.ReadOnly = true;
+            // 
+            // List_Barang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 555);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "List_Barang";
+            this.Text = "List_Barang";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID_BARANG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NAMA_BARANG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DETAIL;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
