71c9d35cd38ad6335aa0b6c2c3781447683c82ea
EnricoPucci01 <49854430+EnricoPucci01@users.noreply.github.com>
2019-11-20 13:41:38 +0700

    commit2

--- Proyek ACS/Form1.cs
+++ Proyek ACS/Form1.cs
@@ -7,29 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
 
 namespace Proyek_ACS
 {
     public partial class Form1 : Form
     {
+        /*
+         * CATATAN :
+         * Jangan lupa remove connection di references lalu add lagi
+         * Lalu ganti data source ke XE kalau pakai EXPRESS
+         * Aku pakai orcl soalnya terlanjur instal enterprise
+         */
+        public static OracleConnection oc = new OracleConnection("User id = projectacs ; password = projectacs ; data source = orcl");
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void koneksi()
+        {
+            try
+            {
+                oc.Open();
+                MessageBox.Show("Sukses Login");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error di : " + ex.Message);
+            }
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("ya allah jalan");
-            MessageBox.Show("Test");
+           
             Form_Main main = new Form_Main();
             this.Hide();
             main.ShowDialog();
             this.Close();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            Database db = new Database("xe","proyekacs","123");
         }
     }
 }
