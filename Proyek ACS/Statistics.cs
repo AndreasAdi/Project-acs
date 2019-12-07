d0d061249ca7898207d6c66f262b916f259776c9
aldylimina <lay1@mhs.stts.edu>
2019-12-07 12:55:40 +0700

    report

--- Proyek ACS/Statistics.cs
+++ Proyek ACS/Statistics.cs
@@ -33,38 +33,42 @@ namespace Proyek_ACS
         private void button1_Click(object sender, EventArgs e)
         {
             CrystalReport2 crpt = new CrystalReport2();
-            crpt.SetDatabaseLogon("proyek", "proyek");
-            //crpt.SetParameterValue("Tanggal_Awal", dateTimePicker1.Value);
-            //crpt.SetParameterValue("Tanggal Akhir", dateTimePicker2.Value);
-            crystalReportViewer1.ReportSource = crpt;
+            
+
             try
             {
-                foreach (CrystalDecisions.CrystalReports.Engine.Table table in crpt.Database.Tables)
-                {
-                    TableLogOnInfo ci = new TableLogOnInfo();
-                    /** 
-                     * @notes Ini itterate di masing-masing tabel pada RPT yang dibuat, sehingga koneksi berubah jadi ini
-                     * Database itu dikosongi agar databasenya tetap seperti sebelumnya
-                     * @see https://stackoverflow.com/q/17914605 
-                     * @see https://stackoverflow.com/questions/4864169/crystal-report-and-problem-with-connection
-                     */
-                    ci.ConnectionInfo.DatabaseName = "";
-                    ci.ConnectionInfo.ServerName = "192.168.1.21"; //ganti ipnya
-                    ci.ConnectionInfo.UserID = "proyek";
-                    ci.ConnectionInfo.Password = "proyek";
-                    table.ApplyLogOnInfo(ci);
-                }
+                //foreach (CrystalDecisions.CrystalReports.Engine.Table table in crpt.Database.Tables)
+                //{
+                //    TableLogOnInfo ci = new TableLogOnInfo();
+                //    /** 
+                //     * @notes Ini itterate di masing-masing tabel pada RPT yang dibuat, sehingga koneksi berubah jadi ini
+                //     * Database itu dikosongi agar databasenya tetap seperti sebelumnya
+                //     * @see https://stackoverflow.com/q/17914605 
+                //     * @see https://stackoverflow.com/questions/4864169/crystal-report-and-problem-with-connection
+                //     */
+                //    ci.ConnectionInfo.DatabaseName = "";
+                //    ci.ConnectionInfo.ServerName = "10.11.238.40"; //ganti ipnya
+                //    ci.ConnectionInfo.UserID = "proyek";
+                //    ci.ConnectionInfo.Password = "proyek";
+                //    table.ApplyLogOnInfo(ci);
+                //}
                 
-               
+
             }
             catch (Exception)
             {
 
                 throw;
             }
+            crpt.SetDatabaseLogon("proyek", "proyek");
+            crystalReportViewer1.ReportSource = crpt;
 
 
+            crpt.SetParameterValue("Tanggal_Awal", dateTimePicker1.Value);
+            crpt.SetParameterValue("Tanggal_Akhir", dateTimePicker2.Value);
             
+
+
         }
 
         private void Statistics_Load(object sender, EventArgs e)
